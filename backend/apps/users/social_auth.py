1c47f0cb20f0b6fee3db1957f4333b12b8dc7156
Radin Amri <radin.amrihesari@gmail.com>
2026-02-26 21:22:19 +0330

    fix: allow public access to Google OAuth endpoint

--- backend/apps/users/social_auth.py
+++ backend/apps/users/social_auth.py
@@ -1,10 +1,12 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
+from rest_framework.permissions import AllowAny
 from decouple import config
 
 
 class GoogleLogin(SocialLoginView):
+    permission_classes = [AllowAny]
     adapter_class = GoogleOAuth2Adapter
     callback_url = config(
         "GOOGLE_CALLBACK_URL",
